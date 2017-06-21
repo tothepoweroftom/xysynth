78e7a651f95c0f0a1faf5313a95973c001833242
Tom Power <tothepoweroftom@gmail.com>
2017-06-21 16:51:54 +0200

    Added audio!!!!!!!!!!!!@£!@£

---
+++ ToneDrone.js
@@ -0,0 +1,36 @@
+function ToneDrone() {
+    this.synth = new Tone.FMSynth({
+
+        "harmonicity": 1.11,
+        "modulationIndex": 5,
+        "detune": 0,
+        "oscillator": {
+            "type": "sine"
+        },
+        "modulation": {
+            "type": "sine"
+        },
+        "modulationEnvelope": {
+            "attack": 0.5,
+            "decay": 0,
+            "sustain": 10,
+            "release": 0.2,
+        }
+    });
+    this.noise = new Tone.Noise("pink");
+    this.limiter = new Tone.Limiter(-6);
+    this.eq = new Tone.EQ3(-10, -20, -1);
+    this.dist = new Tone.Distortion(0.9);
+    this.pingPong = new Tone.PingPongDelay("4n", 0.7);
+    this.reverb = new Tone.Freeverb(0.1, 500);
+    this.vol = new Tone.Volume(0).toMaster();
+}
+
+ToneDrone.prototype.connectComponents = function() {
+    this.synth.chain(this.eq, this.pingPong, this.reverb, this.dist, this.limiter, this.vol);
+}
+
+ToneDrone.prototype.play = function() {
+    this.synth.triggerAttackRelease("C1");
+
+}
--- sketch.js
+++ sketch.js
@@ -3,22 +3,36 @@ var XYModel;
 var canvas;
 var slider1, slider2, slider3;
 var T,J,B;
+var button;
+var toneDrone;
 function setup() {
   colorMode(HSB);
   canvas = createCanvas(windowWidth*0.5,windowWidth*0.5);
   noStroke();
   XYModel = new XYModel(windowWidth*0.5*(1/64), 64, canvas);
   XYModel.randomInit();
+
+  toneDrone = new ToneDrone();
+  toneDrone.connectComponents();
   slider1 = createSlider(0.001,1.0,0.01, 0.001);
   slider2 = createSlider(-1.0,1.0,0.0, 0.01);
   slider3 = createSlider(-1.0,1.0,0.0, 0.01);
 
-  var x = (windowWidth - width) / 2;
+  button = createButton('click me');
+  button.position(19, 19);
+  button.mousePressed(play);
+
+    var x = (windowWidth - width) / 2;
   var y = (windowHeight - height) / 2;
   canvas.position(x, y);
 
   frameRate(30);
 
+  var loop = new Tone.Loop(function(time){
+  	//triggered every eighth note.
+    play();
+  }, "8n").start(0);
+  Tone.Transport.start();
 }
 
 function draw() {
@@ -29,7 +43,8 @@ rect(0,0, width, height);
   XYModel.T = slider1.value();
  XYModel.J = slider2.value();
    XYModel.B = slider3.value();
-
+   toneDrone.synth.harmonicity.value = map(slider1.value(), 0.001, 1.0, 1.0, 10.0);
+   toneDrone.synth.modulationIndex.value = map(slider1.value(), 0.001, 1.0, 1.0, 100.0);
 
 
 }
@@ -37,7 +52,7 @@ rect(0,0, width, height);
 function update_temp(){
   let gTval = parseFloat(document.getElementById('temp').value);
   XYModel.T = gTval;
-  console.log(gTval);
+
 
 }
 function update_field(){
@@ -51,4 +66,8 @@ function update_K(){
   XYModel.J = gJval;
   console.log(gJval);
 
+}
+
+function play(){
+  toneDrone.play();
 }
