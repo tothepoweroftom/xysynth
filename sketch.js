498b7cd7178c35e1e2e4b591003f9f8226efa4ed
tothepoweroftom <powerth@tcd.ie>
2017-08-23 13:46:40 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -29,8 +29,8 @@ function setup() {
     // Tone.Master.volume.rampTo(-1, 0.5);
 
     slider1 = createSlider(0.001, 1.0, 0.1, 0.001);
-    slider2 = createSlider(-1.0, 1.0, 0.0, 0.01);
-    slider3 = createSlider(-1.0, 1.0, 0.0, 0.01);
+    slider2 = createSlider(-0.5, 0.5, 0.0, 0.01);
+    slider3 = createSlider(-0.5, 0.5, 0.0, 0.01);
     slider4 = createSlider(0.0, 100.0, 0.0, 1.0);
     button = createButton('Start');
 		button.id("start");
